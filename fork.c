d26c21ee7fd08c6d1f68c0bf0c23e3a66c13cfc1
janahmed <jan.ahmed.prg@gmail.com>
2023-03-07 21:47:20 +0000

    starter

--- fork.c
+++ fork.c
@@ -4,7 +4,18 @@
 #include <sys/wait.h>
 #include <unistd.h>
 int main() {
-    int status;
-    int pid = fork();
+    pid_t parent, child, grandchild;
+    parent = getpid();
+    fork();
+    child = getpid();
+    if (child != parent){
+        fork();
+        grandchild = getpid();
+        if(grandchild != child){
+            printf("I am the grandchild (#%d), where ‘#’ is the pid of the grandchild process\n",grandchild);
+            printf("I am the child (#%d), where ‘#’ is the pid of the child process\n", child);
+            printf("I am the parent (#%d), where ‘#’ is the pid of the parent process\n",parent);
+        }
+    }
     return 0;
 }
